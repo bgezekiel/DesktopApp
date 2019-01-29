f0ff4062a137c53593eda42933d3ad93a10e98f1
yuuuchu <44778601+yuuuchu@users.noreply.github.com>
2019-01-29 13:58:09 -0700

    Update ViewPackages.Designer.cs

--- ThreadedProject2/ViewPackages.Designer.cs
+++ ThreadedProject2/ViewPackages.Designer.cs
@@ -58,7 +58,7 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.Khaki;
-            this.label1.Location = new System.Drawing.Point(9, 29);
+            this.label1.Location = new System.Drawing.Point(8, 23);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(107, 29);
             this.label1.TabIndex = 0;
@@ -69,15 +69,14 @@
             this.groupBox1.BackColor = System.Drawing.SystemColors.ButtonFace;
             this.groupBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("groupBox1.BackgroundImage")));
             this.groupBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.groupBox1.Controls.Add(this.richTextBox1);
             this.groupBox1.Controls.Add(this.button2);
             this.groupBox1.Controls.Add(this.btnBack);
             this.groupBox1.Controls.Add(this.groupBox2);
+            this.groupBox1.Controls.Add(this.btnEdit);
             this.groupBox1.Controls.Add(this.lblViewProducts);
-            this.groupBox1.Controls.Add(this.lblViewPackages);
             this.groupBox1.Controls.Add(this.lstPackageProducts);
             this.groupBox1.Controls.Add(this.lblPackages);
-            this.groupBox1.Location = new System.Drawing.Point(3, -4);
+            this.groupBox1.Location = new System.Drawing.Point(3, -3);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(794, 454);
             this.groupBox1.TabIndex = 2;
@@ -87,9 +86,9 @@
             // 
             this.richTextBox1.BackColor = System.Drawing.SystemColors.InactiveCaption;
             this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.richTextBox1.Location = new System.Drawing.Point(417, 105);
+            this.richTextBox1.Location = new System.Drawing.Point(6, 240);
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(342, 158);
+            this.richTextBox1.Size = new System.Drawing.Size(372, 64);
             this.richTextBox1.TabIndex = 19;
             this.richTextBox1.Text = "";
             // 
@@ -99,7 +98,7 @@
             this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button2.Location = new System.Drawing.Point(220, 381);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(172, 37);
+            this.button2.Size = new System.Drawing.Size(172, 48);
             this.button2.TabIndex = 18;
             this.button2.Text = "Exit Program";
             this.button2.UseVisualStyleBackColor = false;
@@ -111,7 +110,7 @@
             this.btnBack.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnBack.Location = new System.Drawing.Point(20, 381);
             this.btnBack.Name = "btnBack";
-            this.btnBack.Size = new System.Drawing.Size(172, 37);
+            this.btnBack.Size = new System.Drawing.Size(172, 48);
             this.btnBack.TabIndex = 17;
             this.btnBack.Text = "Back";
             this.btnBack.UseVisualStyleBackColor = false;
@@ -120,33 +119,34 @@
             // groupBox2
             // 
             this.groupBox2.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox2.Controls.Add(this.richTextBox1);
             this.groupBox2.Controls.Add(this.dtEnd);
             this.groupBox2.Controls.Add(this.dtStart);
             this.groupBox2.Controls.Add(this.label4);
             this.groupBox2.Controls.Add(this.comboBox1);
-            this.groupBox2.Controls.Add(this.btnEdit);
             this.groupBox2.Controls.Add(this.txtPrice);
             this.groupBox2.Controls.Add(this.txtName);
+            this.groupBox2.Controls.Add(this.lblViewPackages);
             this.groupBox2.Controls.Add(this.label5);
             this.groupBox2.Controls.Add(this.label2);
             this.groupBox2.Controls.Add(this.lblPackageName);
             this.groupBox2.Controls.Add(this.label1);
-            this.groupBox2.Location = new System.Drawing.Point(8, 76);
+            this.groupBox2.Location = new System.Drawing.Point(8, 65);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(384, 279);
+            this.groupBox2.Size = new System.Drawing.Size(384, 310);
             this.groupBox2.TabIndex = 16;
             this.groupBox2.TabStop = false;
             // 
             // dtEnd
             // 
-            this.dtEnd.Location = new System.Drawing.Point(143, 201);
+            this.dtEnd.Location = new System.Drawing.Point(143, 145);
             this.dtEnd.Name = "dtEnd";
             this.dtEnd.Size = new System.Drawing.Size(233, 20);
             this.dtEnd.TabIndex = 19;
             // 
             // dtStart
             // 
-            this.dtStart.Location = new System.Drawing.Point(143, 175);
+            this.dtStart.Location = new System.Drawing.Point(143, 119);
             this.dtStart.Name = "dtStart";
             this.dtStart.Size = new System.Drawing.Size(233, 20);
             this.dtStart.TabIndex = 18;
@@ -156,7 +156,7 @@
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.ForeColor = System.Drawing.Color.Khaki;
-            this.label4.Location = new System.Drawing.Point(46, 198);
+            this.label4.Location = new System.Drawing.Point(46, 142);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(100, 20);
             this.label4.TabIndex = 17;
@@ -167,7 +167,7 @@
             this.comboBox1.BackColor = System.Drawing.SystemColors.InactiveCaption;
             this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(116, 29);
+            this.comboBox1.Location = new System.Drawing.Point(116, 23);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(253, 32);
             this.comboBox1.TabIndex = 16;
@@ -177,9 +177,9 @@
             // 
             this.btnEdit.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(82)))), ((int)(((byte)(117)))), ((int)(((byte)(167)))));
             this.btnEdit.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnEdit.Location = new System.Drawing.Point(162, 76);
+            this.btnEdit.Location = new System.Drawing.Point(514, 381);
             this.btnEdit.Name = "btnEdit";
-            this.btnEdit.Size = new System.Drawing.Size(172, 37);
+            this.btnEdit.Size = new System.Drawing.Size(172, 48);
             this.btnEdit.TabIndex = 15;
             this.btnEdit.Text = "Edit";
             this.btnEdit.UseVisualStyleBackColor = false;
@@ -187,19 +187,19 @@
             // txtPrice
             // 
             this.txtPrice.BackColor = System.Drawing.SystemColors.InactiveCaption;
-            this.txtPrice.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtPrice.Location = new System.Drawing.Point(143, 235);
+            this.txtPrice.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPrice.Location = new System.Drawing.Point(142, 174);
             this.txtPrice.Name = "txtPrice";
-            this.txtPrice.Size = new System.Drawing.Size(235, 35);
+            this.txtPrice.Size = new System.Drawing.Size(235, 29);
             this.txtPrice.TabIndex = 14;
             // 
             // txtName
             // 
             this.txtName.BackColor = System.Drawing.SystemColors.InactiveCaption;
-            this.txtName.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtName.Location = new System.Drawing.Point(143, 127);
+            this.txtName.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtName.Location = new System.Drawing.Point(143, 81);
             this.txtName.Name = "txtName";
-            this.txtName.Size = new System.Drawing.Size(235, 35);
+            this.txtName.Size = new System.Drawing.Size(235, 29);
             this.txtName.TabIndex = 12;
             // 
             // label5
@@ -207,7 +207,7 @@
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label5.ForeColor = System.Drawing.Color.Khaki;
-            this.label5.Location = new System.Drawing.Point(9, 243);
+            this.label5.Location = new System.Drawing.Point(8, 178);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(138, 20);
             this.label5.TabIndex = 11;
@@ -218,7 +218,7 @@
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.Khaki;
-            this.label2.Location = new System.Drawing.Point(38, 173);
+            this.label2.Location = new System.Drawing.Point(38, 117);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(108, 20);
             this.label2.TabIndex = 10;
@@ -229,7 +229,7 @@
             this.lblPackageName.AutoSize = true;
             this.lblPackageName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblPackageName.ForeColor = System.Drawing.Color.Khaki;
-            this.lblPackageName.Location = new System.Drawing.Point(2, 137);
+            this.lblPackageName.Location = new System.Drawing.Point(2, 85);
             this.lblPackageName.Name = "lblPackageName";
             this.lblPackageName.Size = new System.Drawing.Size(144, 20);
             this.lblPackageName.TabIndex = 7;
@@ -241,7 +241,7 @@
             this.lblViewProducts.BackColor = System.Drawing.Color.Transparent;
             this.lblViewProducts.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblViewProducts.ForeColor = System.Drawing.Color.Khaki;
-            this.lblViewProducts.Location = new System.Drawing.Point(413, 274);
+            this.lblViewProducts.Location = new System.Drawing.Point(413, 42);
             this.lblViewProducts.Name = "lblViewProducts";
             this.lblViewProducts.Size = new System.Drawing.Size(172, 20);
             this.lblViewProducts.TabIndex = 6;
@@ -253,7 +253,7 @@
             this.lblViewPackages.BackColor = System.Drawing.Color.Transparent;
             this.lblViewPackages.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblViewPackages.ForeColor = System.Drawing.Color.Khaki;
-            this.lblViewPackages.Location = new System.Drawing.Point(413, 82);
+            this.lblViewPackages.Location = new System.Drawing.Point(5, 217);
             this.lblViewPackages.Name = "lblViewPackages";
             this.lblViewPackages.Size = new System.Drawing.Size(100, 20);
             this.lblViewPackages.TabIndex = 5;
@@ -265,9 +265,9 @@
             this.lstPackageProducts.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lstPackageProducts.FormattingEnabled = true;
             this.lstPackageProducts.ItemHeight = 16;
-            this.lstPackageProducts.Location = new System.Drawing.Point(417, 297);
+            this.lstPackageProducts.Location = new System.Drawing.Point(417, 65);
             this.lstPackageProducts.Name = "lstPackageProducts";
-            this.lstPackageProducts.Size = new System.Drawing.Size(342, 116);
+            this.lstPackageProducts.Size = new System.Drawing.Size(342, 308);
             this.lstPackageProducts.TabIndex = 4;
             // 
             // lblPackages
