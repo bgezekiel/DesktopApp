00a7b777b04a4f2291b9a3f8f3427877b508fb02
bgezekiel <44778576+bgezekiel@users.noreply.github.com>
2019-01-30 08:45:11 -0700

    Create Products Class

---
+++ DBClasses/Products.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/**
+ * Project: OOSD Threaded Project 2
+* Package class with getters and setters
+* Author: Brandon Ezekiel
+* Date: Jan 2019
+* Commenter: Brandon Ezekiel
+*/
+
+namespace DBClasses
+{
+    // Make product class
+    public class Products
+    {
+        // public methods for get and set
+
+        public Products() { } 
+
+        public int ProductId { get; set; }
+
+        public string ProdName { get; set; }
+    }
+}
